b63e158737326585ce77b1da2665187013745b57
Digamber Singh <admin@remotestack.io>
2020-12-16 22:06:47 +0530

    first commit

---
+++ backend/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JwtAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/signup', [JwtAuthController::class, 'register']);
+    Route::post('/signin', [JwtAuthController::class, 'login']);
+    Route::get('/user', [JwtAuthController::class, 'user']);
+    Route::post('/token-refresh', [JwtAuthController::class, 'refresh']);
+    Route::post('/signout', [JwtAuthController::class, 'signout']);
+});
+
